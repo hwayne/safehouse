06487d17dc3b88a08ee7ff4e4ada661369c828ea
Hillel Wayne <hwayne@gmail.com>
2014-08-22 17:57:59 -0700

    add config file for tests

---
+++ sms/tests/config.py
@@ -0,0 +1,8 @@
+# Testing environment
+
+from os import environ
+
+environ['MY_NUMBER'] = '6655321'
+environ['MY_NAME'] = 'YESOCH'
+environ['TWILIO_NUMBER'] = '15005550006'
+environ['DJANGO_DEBUG'] = 'True'
