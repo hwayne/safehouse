ba032b58fbf9ec956d89d6cf006003631a7a40f7
Hillel Wayne <hwayne@gmail.com>
2014-09-13 22:20:01 -0700

    pluralization, appifying

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from journal.models import Entry
 
-# Register your models here.
+admin.site.register(Entry)
--- journal/models.py
+++ journal/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Entry(models.Model):
-    tag = models.CharField(max_length=16)
+    class Meta:
+        verbose_name_plural = "Entries"
+    name = models.CharField(max_length=16)
     rating = models.IntegerField(null=True) # Maximum
     comment = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} - {} - {}".format(
+        return "{} - {} - {}".format(self.name, self.rating, self.comment)
 
